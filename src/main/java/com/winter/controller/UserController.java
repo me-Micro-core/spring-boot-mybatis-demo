caf62565ab4f6be2892af2714137a7fe6c18e694
me-Micro-core <1248882527@qq.com>
2018-10-28 09:04:00 +0800

    添加thymeleaf整合

--- src/main/java/com/winter/controller/UserController.java
+++ src/main/java/com/winter/controller/UserController.java
@@ -1,9 +1,10 @@
-package com.winter.Controller;
+package com.winter.controller;
 
 import com.winter.model.User;
 import com.winter.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,5 +38,12 @@ public class UserController {
     @RequestMapping(value = "/getAll", produces = {"application/json;charset=UTF-8"})
     public List<User> getAll(){
         return userService.findAllUser(1,0);
+    }
+
+    @RequestMapping(value = "/redirectUse")
+    public String redirectUse(Model model){
+        List<User> userList = userService.findAllUser(1,0);
+        model.addAttribute("userList",userList);
+        return "user/userList";
     }
 }
